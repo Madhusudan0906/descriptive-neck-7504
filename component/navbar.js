60235e7e0e2c811ea4fd44e2da5f460e5e51f774
GandhamRaghavendra <graghavendramurty13@gmail.com>
2022-07-22 11:44:47 +0530

    footer added

---
+++ component/navbar.js
@@ -0,0 +1,21 @@
+let header = () => {
+  return `      <div id="navbar">
+        <div id="logodiv">
+          <a href=""
+            ><img
+              src="https://res.cloudinary.com/urbanclap/image/upload/images/growth/home-screen/1631097450980-d2de38.png"
+              alt="company logo"
+          /></a>
+        </div>
+        <div id="loginSignupdiv">
+          <a href="">Blog</a>
+          <a href=""><u>Register As A Professional</u></a>
+          <div>
+            <a id="login" href="">Login</a><a id="signup" href="">Sign Up</a>
+          </div>
+        </div>
+      </div>`;
+};
+
+
+export { header };
