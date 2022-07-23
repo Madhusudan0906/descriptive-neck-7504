e76414ee9f861285ca35de5dd9870a030eb853b8
srinivas ch <srinivasnani3384@gmail.com>
2022-07-23 09:14:52 +0530

    day-4 cartpage almost complete

--- cart/cartpage.js
+++ cart/cartpage.js
@@ -1,14 +1,35 @@
 
-let products = JSON.parse(localStorage.getItem("womenSlonArr")) || [];
-console.log(products)
-// function append(){
+let products = JSON.parse(localStorage.getItem("womenSlonArr")) 
+
+
+let cartTotal = products.reduce(function(sum,elem){
+    return sum + Number(elem.price);
+},0)
+console.log(cartTotal)
+
+
+
+
+
+
+
+
+    document.getElementById("provalue").innerText = cartTotal;
+//window.location.reload();
+//console.log(products)
+
+
+ function append(elem){
+
     products.forEach(function(elem){
+        console.log(elem)
         let btn = document.getElementById("remove").addEventListener("click",sub)
         let btn1 = document.getElementById("add").addEventListener("click",add)
         // let div = document.createElement("div")
         
-        // let image = document.createElement("img")
-        // image.src=elem.image;
+        let image = document.createElement("img")
+        image.src=elem.image;
+        console.log(image)
 
         // let price = document.createElement("p")
         // price.innerText = elem.price;
@@ -17,18 +38,24 @@ console.log(products)
 
         // document.getElementById("box").append(div)
     
-        document.getElementById("provalue").innerText = "₹"+elem.price;
-        document.getElementById("strikevalue").innerText = "₹"+elem.price;
+        //document.getElementById("provalue").innerText = "₹"+elem.price;
+        //document.getElementById("strikevalue").innerText = "₹"+elem.price;
 
         function sub(){
-           document.getElementById("provalue").innerText = `₹${elem.price - Number(299)}`;
+           document.getElementById("provalue").innerText = `₹${cartTotal - Number(299)}`;
         //console.log(elem.price)
         }
         function add(){
-            document.getElementById("provalue").innerText = `₹${elem.price - Number(199)}`;
+            document.getElementById("provalue").innerText = `₹${cartTotal - Number(199)}`;
          //console.log(elem.price)
          }
+         
+        
     })
+}
+        append()
+    
+    
 // ------------savings value section -------------
 
 document.getElementById("two").addEventListener("click",twofun)
@@ -59,39 +86,40 @@ var value = document.getElementById("savingscost").innerText="₹"+Number(144*12
 }
 
 
-function addimage(){
-    let image = document.getElementsByClassName("content").innerText;
-    //let imagearr = JSON.parse(localStorage.getItem("images")) || [];
-    console.log(image)
-    imagearr.push(image);
-    localStorage.setItem("images", JSON.stringify(imagearr));
-    image.value= null;
-   }
-   let id;
-   let i = 0;
+// function addimage(){
+//     let image = document.getElementById("image").value;
+//     let imagearr = JSON.parse(localStorage.getItem("images")) || [];
+//     imagearr.push(image);
+//     localStorage.setItem("images", JSON.stringify(imagearr));
+//     image.value= null;
+//    }
+//    let id;
+//    let i = 0;
 
-   function slideshow(){
-    let imagearr = JSON.parse(localStorage.getItem("images")) || [];
-    let container = document.getElementById("slideimages")
+//    function slideshow(){
+//     let imagearr = JSON.parse(localStorage.getItem("images")) || [];
+//     let container = document.getElementById("slideimages")
 
-    id = setInterval(function(){
-        console.log(i)
-        if(i == imagearr.length){
-            i=0;
-        }
-        let image = document.createElement("img")
-        image.src= imagearr[i];
-        container.innerHTML = null;
-        container.append(image);
-        i++;
-    },1000)
-}
-function pause(){
-        clearInterval(id)
-    }
-addimage()
+//     id = setInterval(function(){
+//         console.log(i)
+//         if(i == imagearr.length){
+//             i=0;
+//         }
+//         let image = document.createElement("img")
+//         image.src= imagearr[i];
+//         container.innerHTML = null;
+//         container.append(image);
+//         i++;
+//     },1000)
+// }
+// function pause(){
+//         clearInterval(id)
+//     }
+
 
 //console.log(btn)
+
+
 
 
 
